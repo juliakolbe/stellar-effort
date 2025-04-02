9a50315d48624323adc0bddc08635646347a91b4
Noah Kelly <143432785+KellyNJ@users.noreply.github.com>
2025-04-02 19:28:46 -0400

    made corrections noted in the groupme chat

--- lunarLander/Algorithms1.py
+++ lunarLander/Algorithms1.py
@@ -1,14 +1,12 @@
 import math
 from collections import deque
-
 # Vector2D
 class Vector2D:
-    def init(self, x = 0.0, y = 0.0):
+    def __init__(self, x = 0.0, y = 0.0):
         self.x = x
         self.y = y
     def magnitude(self):
         return math.sqrt(self.x * self.x + self.y * self.y)
-
 # Physical constants
 class PhysicsConstants:
     G = 6.67430e-11
@@ -16,51 +14,58 @@ class PhysicsConstants:
     moon_radius = 1.740e6
     safe_landing_velocity = 5.0
     time_step = 1.0
-
 # Lander state
 class LanderState:
-    def init(self, empty_mass, initial_fuel):
+    def __init__(self, empty_mass, initial_fuel):
         self.empty_mass = empty_mass
         self.fuel_mass = initial_fuel
         self.mass = empty_mass + initial_fuel
         self.altitude = 0.0
         self.velocity = Vector2D()
-        self.velocity.init(0.0, 0.0)
+        self.velocity.__init__(0.0, 0.0)
         self.acceleration = Vector2D()
-        self.acceleration.init(0.0, 0.0)
-
+        self.acceleration.__init__(0.0, 0.0)
 # Thrust control
 class ThrustControl:
-    def init(self):
+    def __init__(self):
         self.thrust_force = 0.0
         self.burn_efficiency = 1.0
     def apply_thrust(self, lander, duration):
         fuel_used = (self.thrust_force / self.burn_efficiency) *  duration
         lander.fuel_mass -= fuel_used
         lander.mass = lander.empty_mass + lander.fuel_mass
-
 # Lander "Doom"
 class DoomLander:
-    def init(self, empty_mass, initial_fuel, initial_altitude):
+    def __init__(self, empty_mass, initial_fuel, initial_altitude):
         self.state = LanderState()
-        self.state.init(empty_mass, initial_fuel)
+        self.state.__init__(empty_mass, initial_fuel)
         self.state.altitude = initial_altitude
         self.thruster = ThrustControl()
-        self.thruster.init()
+        self.thruster.__init__()
         self.thrust_commands = deque()
     def calculate_gravitational_acceleration(self):
         r = PhysicsConstants.moon_radius + self.state.altitude
-        return (PhysicsConstants.G * PhysicsConstants.moon_mass) / (r * r)
+        # Add negative sign to make gravity pull downward
+        return -1 * (PhysicsConstants.G * PhysicsConstants.moon_mass) / (r * r)
     def update(self):
         g = self.calculate_gravitational_acceleration()
         if self.thrust_commands:
             thrust = self.thrust_commands.popleft()
             self.thruster.thrust_force = thrust
-        net_accel = (self.thruster.thrust_force - g * self.state.mass) / \
-            (self.state.mass - 0.5 * self.thruster.thrust_force / self.thruster.burn_efficiency)
+        
+        # Calculate net acceleration (positive means upward)
+        # Thrust is upward (positive), gravity is downward (negative)
+        net_accel = self.thruster.thrust_force / self.state.mass + g
+        
         dt = PhysicsConstants.time_step
-        self.state.altitude = self.state.altitude - self.state.velocity.y * dt - 0.5 * net_accel * dt * dt
+        
+        # First update velocity using current acceleration
         self.state.velocity.y = self.state.velocity.y + net_accel * dt
+        
+        # Then update altitude using new velocity
+        self.state.altitude = self.state.altitude + self.state.velocity.y * dt
+        
+        # Apply thrust effects on fuel if thrusting
         if self.thruster.thrust_force > 0:
             self.thruster.apply_thrust(self.state, dt)
     def queue_thrust_command(self, thrust):
